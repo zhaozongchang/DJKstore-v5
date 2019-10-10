d36309e004eabffe4adb4e948adb1e6e87705035
赵宗昌 <xingyingsuifeng@gmail.com>
2019-10-10 17:57:33 +0800

    add add_to_cart button

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -6,4 +6,11 @@ class ProductsController < ApplicationController
   def show
     @product = Product.find(params[:id])
   end
+
+  def add_to_cart
+     @product = Product.find(params[:id])
+     redirect_back fallback_location: root_path
+     # redirect_to product_path
+     flash[:notice] = "测试加入购物车"
+   end
 end
