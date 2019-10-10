2974a8d9ff1ad8833c86af7339790d86d40b2eb9
赵宗昌 <xingyingsuifeng@gmail.com>
2019-10-10 19:08:44 +0800

    add admin/orders

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_10_104833) do
+ActiveRecord::Schema.define(version: 2019_10_10_110404) do
 
   create_table "cart_items", force: :cascade do |t|
     t.integer "cart_id"
@@ -37,6 +37,8 @@ ActiveRecord::Schema.define(version: 2019_10_10_104833) do
     t.string "token"
     t.boolean "is_paid", default: false
     t.string "payment_method"
+    t.string "aasm_state", default: "order_placed"
+    t.index ["aasm_state"], name: "index_orders_on_aasm_state"
   end
 
   create_table "product_lists", force: :cascade do |t|
